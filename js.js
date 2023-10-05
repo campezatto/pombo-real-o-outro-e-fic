49d1a75101fa4426aade0939f4b11e475b581609
cnidario <128915347+campezatto@users.noreply.github.com>
2023-10-05 16:39:56 -0300

    FINAL DESSA BOSTA

--- js.js
+++ js.js
@@ -9,3 +9,15 @@ function mudaAba1(){
     window.open("Pombanicobar-1.html");
 
 }
+
+function mudaAba2(){
+
+    window.open("pomba do cabo-1.html");
+
+}
+
+function mudaAba3(){
+
+    window.open("pomba frill-1.html");
+
+}
